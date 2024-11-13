9ca55de1094914a05b9397aa915f252e1ba44ef8
= <=>
2024-11-13 12:08:51 +0100

    13/11/2024

---
+++ array100ale/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Random Aleatorio = new Random();
+        int NumAle = Aleatorio.nextInt(1000);
+        int[]array = new int[100];
+        for (int i = 0; i < array.length ; i++) {
+            array[i] = NumAle;
+        }
+        int
+    }
+}
